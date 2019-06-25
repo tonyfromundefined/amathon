99c0a1947d99e183815145586c89d961bf87724c
tonyfromundefined <tony@erion.kr>
2019-06-25 15:05:44 +0900

    remove in-seoul

--- src/components/Stories/Copyright.tsx
+++ src/components/Stories/Copyright.tsx
@@ -1,4 +1,3 @@
-import InSeoulImage from '~/assets/story-copyright-in-seoul.svg'
 import LabelImage from '~/assets/story-copyright-label.svg'
 import styled from '~/styled'
 import Section from '../System/Section'
@@ -13,7 +12,6 @@ export default function StoryCopyright() {
         >
           <Label src={LabelImage} />
         </LabelContainer>
-        <InSeoul src={InSeoulImage} />
       </Labels>
     </Section>
   )
@@ -39,10 +37,4 @@ const LabelContainer = styled.a`
 const Label = styled.img`
   width: 9.0625rem;
   height: .9375rem;
-`
-
-const InSeoul = styled.img`
-  width: 3.125rem;
-  height: .75rem;
-  opacity: .2;
 `
